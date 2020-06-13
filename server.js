3bd4bb84d45d5b5b77497bbaeab43c4c569bf21b
Habib Allah Kechout <fm_kechout@esi.dz>
2020-06-14 00:58:29 +0100

    deployment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist/tpgo3'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/tpgo1/index.html'));
+res.sendFile(path.join(__dirname+'/dist/tpgo3/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
